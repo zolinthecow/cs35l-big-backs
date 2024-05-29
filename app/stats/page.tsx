3be8926142f93795746f4fd4fb184968f55d9130
VivekGarg2004 <vivekgarg2004@gmail.com>
2024-05-28 21:00:04 -0700

    Revert "publish vivek/stats-page along with basic changes"
    This reverts commit be81f579bb7037706c7890bdb524eb915f8f7580.

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,4 +1,5 @@
-"use client";
+'use client';
+
 import React, { useEffect, useState } from 'react';
 import getSpotifyClient from '@/lib/spotify';
 import { NavBar } from '@/components/ui/navbar';
@@ -25,18 +26,25 @@ const Page: React.FC = () => {
   const [topTracks, setTopTracks] = useState<Track[]>([]);
   const [topArtists, setTopArtists] = useState<Artist[]>([]);
   const [recentlyPlayed, setRecentlyPlayed] = useState<RecentlyPlayed[]>([]);
-  const [timeRange, setTimeRange] = useState<'short_term' | 'medium_term' | 'long_term'>('short_term');
-  const [currentView, setCurrentView] = useState<'tracks' | 'artists' | 'recent'>('tracks');
+  const [timeRange, setTimeRange] = useState<
+    'short_term' | 'medium_term' | 'long_term'
+  >('short_term');
+  const [currentView, setCurrentView] = useState<
+    'tracks' | 'artists' | 'recent'
+  >('tracks');
 
   useEffect(() => {
     const fetchTopTracks = async () => {
       try {
         const { accessToken } = await getSpotifyClient();
-        const response = await fetch(`https://api.spotify.com/v1/me/top/tracks?time_range=${timeRange}&limit=20`, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
+        const response = await fetch(
+          `https://api.spotify.com/v1/me/top/tracks?time_range=${timeRange}&limit=20`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
           },
-        });
+        );
         const data = await response.json();
         setTopTracks(data.items);
       } catch (error) {
@@ -47,11 +55,14 @@ const Page: React.FC = () => {
     const fetchTopArtists = async () => {
       try {
         const { accessToken } = await getSpotifyClient();
-        const response = await fetch(`https://api.spotify.com/v1/me/top/artists?time_range=${timeRange}&limit=20`, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
+        const response = await fetch(
+          `https://api.spotify.com/v1/me/top/artists?time_range=${timeRange}&limit=20`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
           },
-        });
+        );
         const data = await response.json();
         setTopArtists(data.items);
       } catch (error) {
@@ -62,11 +73,14 @@ const Page: React.FC = () => {
     const fetchRecentlyPlayed = async () => {
       try {
         const { accessToken } = await getSpotifyClient();
-        const response = await fetch(`https://api.spotify.com/v1/me/player/recently-played?limit=20`, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
+        const response = await fetch(
+          `https://api.spotify.com/v1/me/player/recently-played?limit=20`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
           },
-        });
+        );
         const data = await response.json();
         setRecentlyPlayed(data.items);
       } catch (error) {
@@ -85,91 +99,160 @@ const Page: React.FC = () => {
 
   return (
     <div className="bg-white">
+            
       <NavBar />
+            
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                
         <div className="flex items-center justify-center border-b-2 border-gray-100 py-6 md:justify-center md:space-x-10">
+                    
           <nav className="hidden md:flex space-x-10">
+                        
             <button
               className={`text-base font-medium text-gray-500 hover:text-gray-900 ${currentView === 'tracks' ? 'text-black' : ''}`}
               onClick={() => setCurrentView('tracks')}
             >
-              Top Tracks
+                            Top Tracks             
             </button>
+                        
             <button
               className={`text-base font-medium text-gray-500 hover:text-gray-900 ${currentView === 'artists' ? 'text-black' : ''}`}
               onClick={() => setCurrentView('artists')}
             >
-              Top Artists
+                            Top Artists             
             </button>
+                        
             <button
               className={`text-base font-medium text-gray-500 hover:text-gray-900 ${currentView === 'recent' ? 'text-black' : ''}`}
               onClick={() => setCurrentView('recent')}
             >
-              Recently Played
+                            Recently Played             
             </button>
+                      
           </nav>
+                  
         </div>
+              
       </div>
+            
       <div className="py-10">
+                
         <header className="text-center">
+                    
           <h1 className="text-4xl font-bold leading-tight text-gray-900">
-            {currentView === 'tracks' && 'Top Tracks'}
-            {currentView === 'artists' && 'Top Artists'}
-            {currentView === 'recent' && 'Recently Played'}
+                        {currentView === 'tracks' && 'Top Tracks'}
+                        {currentView === 'artists' && 'Top Artists'}
+                        {currentView === 'recent' && 'Recently Played'}
+                      
           </h1>
+                  
         </header>
+                
         {(currentView === 'tracks' || currentView === 'artists') && (
           <div className="flex justify-center space-x-4 my-4">
+                        
             <button
               className={`px-4 py-2 ${timeRange === 'short_term' ? 'bg-gray-700 text-white' : 'bg-gray-200 text-gray-700'}`}
               onClick={() => setTimeRange('short_term')}
             >
-              Last 4 weeks
+                            Last 4 weeks             
             </button>
+                        
             <button
               className={`px-4 py-2 ${timeRange === 'medium_term' ? 'bg-gray-700 text-white' : 'bg-gray-200 text-gray-700'}`}
               onClick={() => setTimeRange('medium_term')}
             >
-              Last 6 months
+                            Last 6 months             
             </button>
+                        
             <button
               className={`px-4 py-2 ${timeRange === 'long_term' ? 'bg-gray-700 text-white' : 'bg-gray-200 text-gray-700'}`}
               onClick={() => setTimeRange('long_term')}
             >
-              Last 12 months
+                            Last 12 months             
             </button>
+                      
           </div>
         )}
+                
         <div className="mt-12 grid grid-cols-3 gap-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          {currentView === 'tracks' && topTracks.map((track) => (
-            <div key={track.id} className="flex items-center space-x-4">
-              <img src={track.album.images[0].url} alt={track.name} width="50" height="50" />
-              <div>
-                <div className="font-medium text-lg">{track.name}</div>
-                <div className="text-sm text-gray-500">{track.artists.map(artist => artist.name).join(', ')}</div>
+                    
+          {currentView === 'tracks' &&
+            topTracks.map((track) => (
+              <div key={track.id} className="flex items-center space-x-4">
+                              
+                <img
+                  src={track.album.images[0].url}
+                  alt={track.name}
+                  width="50"
+                  height="50"
+                />
+                              
+                <div>
+                                  
+                  <div className="font-medium text-lg">{track.name}</div>
+                                  
+                  <div className="text-sm text-gray-500">
+                    {track.artists.map((artist) => artist.name).join(', ')}
+                  </div>
+                                
+                </div>
+                            
               </div>
-            </div>
-          ))}
-          {currentView === 'artists' && topArtists.map((artist) => (
-            <div key={artist.id} className="flex items-center space-x-4">
-              <img src={artist.images[0].url} alt={artist.name} width="50" height="50" />
-              <div>
-                <div className="font-medium text-lg">{artist.name}</div>
+            ))}
+                    
+          {currentView === 'artists' &&
+            topArtists.map((artist) => (
+              <div key={artist.id} className="flex items-center space-x-4">
+                              
+                <img
+                  src={artist.images[0].url}
+                  alt={artist.name}
+                  width="50"
+                  height="50"
+                />
+                              
+                <div>
+                                  
+                  <div className="font-medium text-lg">{artist.name}</div>
+                                
+                </div>
+                            
               </div>
-            </div>
-          ))}
-          {currentView === 'recent' && recentlyPlayed.map((item) => (
-            <div key={item.track.id} className="flex items-center space-x-4">
-              <img src={item.track.album.images[0].url} alt={item.track.name} width="50" height="50" />
-              <div>
-                <div className="font-medium text-lg">{item.track.name}</div>
-                <div className="text-sm text-gray-500">{item.track.artists.map(artist => artist.name).join(', ')}</div>
-                <div className="text-sm text-gray-500">{new Date(item.played_at).toLocaleString()}</div>
+            ))}
+                    
+          {currentView === 'recent' &&
+            recentlyPlayed.map((item) => (
+              <div key={item.track.id} className="flex items-center space-x-4">
+                              
+                <img
+                  src={item.track.album.images[0].url}
+                  alt={item.track.name}
+                  width="50"
+                  height="50"
+                />
+                              
+                <div>
+                                  
+                  <div className="font-medium text-lg">{item.track.name}</div>
+                                  
+                  <div className="text-sm text-gray-500">
+                    {item.track.artists.map((artist) => artist.name).join(', ')}
+                  </div>
+                                  
+                  <div className="text-sm text-gray-500">
+                    {new Date(item.played_at).toLocaleString()}
+                  </div>
+                                
+                </div>
+                            
               </div>
-            </div>
-          ))}
+            ))}
+                  
         </div>
+              
       </div>
+          
     </div>
   );
 };
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import Image from 'next/image';
 import { NotificationIcon } from '@/components/ui/notification';
@@ -7,7 +7,6 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useState } from 'react';
 import Link from 'next/link';
-
 
 const handleSearchSubmit = (event: React.FormEvent) => {
   event.preventDefault();
@@ -21,27 +20,47 @@ interface NavBarProps {
 export function NavBar({ className }: NavBarProps) {
   const [isFocused, setIsFocused] = useState(false);
   return (
-    <div className={`flex gap-7 justify-between items-center py-4 px-6 bg-white ${className}`}>
+    <div
+      className={`flex gap-7 justify-between items-center py-4 px-6 bg-white ${className}`}
+    >
       <div className="flex gap-4 items-center space-x-8 flex-shrink-0">
-        <a className="flex items-center space-x-2" href="/">
+        <a className="flex items-center space-x-2" href="/home">
           <Image src="/image.png" alt="Logo" width={36} height={36} />
         </a>
         <nav className="hidden md:flex space-x-8">
-          <a className="text-base font-medium transition-colors hover:text-blue-500" href="/messages">
+          <a
+            className="text-base font-medium transition-colors hover:text-blue-500"
+            href="/messages"
+          >
             Messages
           </a>
-          <a className="text-base font-medium transition-colors hover:text-blue-500" href="/playlists">
+          <a
+            className="text-base font-medium transition-colors hover:text-blue-500"
+            href="/playlists"
+          >
             Playlists
           </a>
-          <a className="text-base font-medium transition-colors hover:text-blue-500" href="/stats">
+          <a
+            className="text-base font-medium transition-colors hover:text-blue-500"
+            href="/stats"
+          >
             Stats
           </a>
         </nav>
       </div>
-      <form className="flex gap-4 justify-center space-x-8 flex-grow" onSubmit={handleSearchSubmit}>
+      <form
+        className="flex gap-4 justify-center space-x-8 flex-grow"
+        onSubmit={handleSearchSubmit}
+      >
         <div className="relative w-full px-2 flex items-center">
           {!isFocused && (
-            <Image src="/searchicon.webp" alt="SearchIcon" width={20} height={20} className="absolute left-4" />
+            <Image
+              src="/searchicon.webp"
+              alt="SearchIcon"
+              width={20}
+              height={20}
+              className="absolute left-4"
+            />
           )}
           <Input
             className="w-full bg-gray-100 border border-gray-300 focus:ring-2 focus:ring-gray-600 focus:outline-none px-4 py-2 rounded-md text-base pl-10"
@@ -52,16 +71,27 @@ export function NavBar({ className }: NavBarProps) {
           />
         </div>
       </form>
-      <div className='flex  justify-between space-x-3'>
+      <div className="flex  justify-between space-x-3">
         <Button variant="ghost" size="sm" className="px-2 hidden sm:block">
-          <a className="flex items-center space-x-2 transition-colors hover:text-blue-500" href="/notifications">
+          <a
+            className="flex items-center space-x-2 transition-colors hover:text-blue-500"
+            href="/notifications"
+          >
             <NotificationIcon count={98} />
           </a>
         </Button>
         <Link href="/profile">
-            <Button variant="ghost" className="text-base font-medium transition-colors hover:text-blue-500 px-2">
-              <img src="https://avatar.iran.liara.run/public/39" alt="Profile" width={36} height={36} />
-            </Button>
+          <Button
+            variant="ghost"
+            className="text-base font-medium transition-colors hover:text-blue-500 px-2"
+          >
+            <img
+              src="https://avatar.iran.liara.run/public/39"
+              alt="Profile"
+              width={36}
+              height={36}
+            />
+          </Button>
         </Link>
       </div>
     </div>
