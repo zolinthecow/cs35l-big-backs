fe1134ab052ca2f539ee67b748eb184fe09ed90d
Colin Zhao <colinzhao@Colins-MacBook-Pro-Toppings.local>
2024-05-31 21:29:05 -0700

    add custom scopes to login

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -2,8 +2,20 @@ import {
   AfterCallbackAppRoute,
   handleAuth,
   handleCallback,
+  handleLogin,
 } from '@auth0/nextjs-auth0';
 import createUserIfNotExisting from '@/actions/createUserIfNotExisting';
+
+const scopes = [
+  'openid',
+  'profile',
+  'email',
+  'user-read-private',
+  'user-read-email',
+  'user-top-read',
+  'user-read-recently-played',
+  'playlist-read-private',
+];
 
 const afterCallback: AfterCallbackAppRoute = async (req, session, state) => {
   const { user } = session;
@@ -17,5 +29,13 @@ const afterCallback: AfterCallbackAppRoute = async (req, session, state) => {
   return session;
 };
 export const GET = handleAuth({
+  // @ts-expect-error whatever
+  async login(req, res) {
+    return await handleLogin(req, res, {
+      authorizationParams: {
+        scope: scopes.join(' '),
+      },
+    });
+  },
   callback: handleCallback({ afterCallback }),
 });
