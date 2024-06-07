462ab25154ad16a08a180ce0915e61961db796b9
VivekGarg2004 <vivekgarg2004@gmail.com>
2024-06-07 11:42:15 -0700

    pinning and unpinning for dynamic routes as well as protection from unpinning someone else's stuff

---
+++ components/profile_ui/profile-side-bar2.tsx
@@ -0,0 +1,56 @@
+'use client';
+import React, { FC } from 'react';
+import { AvatarImage, AvatarFallback, Avatar } from '@/components/ui/avatar';
+import { Rating } from '@/components/ui/rating';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+export interface ProfileProps {
+  name: string;
+  username: string;
+  profilePicture: string;
+  bio: string;
+  ratingValue: number;
+  friendsCount: number;
+}
+
+const ProfileSideBar: FC<ProfileProps> = ({
+  name,
+  username,
+  profilePicture,
+  bio,
+  ratingValue,
+  friendsCount,
+}) => {
+  return (
+    <div className="flex flex-col h-full items-center gap-6 md:w-1/3 bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md md:sticky md:top-16">
+      <div className="w-full text-left mb-0">
+        <h1 className="text-2xl font-bold mb-0">Profile Page</h1>
+      </div>
+      <div className="flex flex-col items-center space-y-4">
+        <Avatar className="h-40 w-40 md:h-60 md:w-60 border-4 border-white dark:border-gray-900">
+          <AvatarImage alt={username} src={profilePicture} />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <Rating rating={ratingValue} />
+        <div className="text-center space-y-2">
+          <h1 className="text-2xl font-bold">{name}</h1>
+          <div className="flex items-center space-x-2">
+            <p className="text-gray-500 dark:text-gray-400">{username}</p>
+            <span className="text-xl">·</span>
+            <p className="text-gray-500 dark:text-gray-400">
+              {friendsCount} friends
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="w-full text-left mb-1">
+        <h2 className="text-xl font-bold mb-0">Bio</h2>
+        <div className="text-gray-700 dark:text-gray-300">{bio}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileSideBar;
---
+++ components/ui/layouts/artist-layout-profile2.tsx
@@ -0,0 +1,55 @@
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '../button';
+
+type ArtistItemProps = {
+  artist: string;
+  artistImage: string;
+  artistUrl: string;
+  className?: string;
+};
+
+export function ArtistLayoutProfile({
+  artist,
+  artistImage,
+  artistUrl,
+  className,
+}: ArtistItemProps) {
+  return (
+    <div
+      className={`flex flex-col items-center gap-2 p-4 rounded-md transition-all duration-200 ease-in-out hover:bg-gray-100 hover:shadow-xl ${className}`}
+    >
+      <div style={{ width: '100px', height: '100px', position: 'relative' }}>
+        <Image
+          alt={`${artist} cover`}
+          src={artistImage}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-md"
+        />
+      </div>
+      <div className="text-center w-40">
+        <h3 className="font-bold text-[15px] text-gray-900 dark:text-gray-200 truncate text-ellipsis">
+          {artist}
+        </h3>
+      </div>
+      <div className="flex space-x-2">
+        <Link href={artistUrl} passHref>
+          <Button
+            variant="default"
+            size="sm"
+            className="p-2 text-white bg-transparent rounded-full hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75"
+          >
+            <Image
+              src="https://static-00.iconduck.com/assets.00/spotify-icon-2048x2048-n3imyp8e.png"
+              alt="Spotify"
+              width={24}
+              height={24}
+            />
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
