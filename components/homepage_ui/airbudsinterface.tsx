2a80b1b36d647097e31180057318531e8f258afe
zolinthecow <32052672+zolinthecow@users.noreply.github.com>
2024-06-07 21:00:35 -0700

    Delete messaing (#32)
    * remove messaging
    
    * redirect / to /home
    
    * forgot to set permanent
    
    * add recently played to airbuds + delete useless buttons
    
    ---------
    
    Co-authored-by: Colin Zhao <colinzhao@Colins-MacBook-Pro-Toppings.local>

--- components/homepage_ui/airbudsinterface.tsx
+++ components/homepage_ui/airbudsinterface.tsx
@@ -102,7 +102,6 @@ const AirbudsInterface: FC<AirbudsInterfaceProps> = ({
 
   const emojis = ['🔥', '❤️', '🙌', '😍'];
 
-  console.log('YOOOO');
   return (
     <div className="flex flex-col items-center justify-between h-full w-full p-4 snap-center">
       <div className="flex flex-col items-center mt-4">
@@ -131,13 +130,6 @@ const AirbudsInterface: FC<AirbudsInterfaceProps> = ({
         <p className="text-sm text-gray-400">{songArtist}</p>
       </div>
       <div className="flex items-center space-x-4 justify-center">
-        <Button
-          variant="ghost"
-          size="sm"
-          className="text-gray-400 hover:text-white"
-        >
-          <IconMute color="black" size={24} />
-        </Button>
         <Link href={songLink}>
           <Button
             variant="default"
@@ -153,20 +145,7 @@ const AirbudsInterface: FC<AirbudsInterfaceProps> = ({
             <span className="ml-2">Play on Spotify</span>
           </Button>
         </Link>
-        <Button
-          variant="ghost"
-          size="sm"
-          className="text-gray-400 hover:text-white"
-        >
-          <IconAdd color="black" size={24} />
-        </Button>
       </div>
-      <div className="flex justify-around w-full mt-6 px-8">
-        {emojis.map((emoji) => (
-          <EmojiButton key={emoji} emoji={emoji} />
-        ))}
-      </div>
-      <Reply sendReply={sendReply} />
     </div>
   );
 };
